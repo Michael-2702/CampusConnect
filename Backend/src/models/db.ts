47904275ad5bbcc66e9a58d6df4e6752c04294b7
Michael Hosamani <michaelhosamani26@gmail.com>
2024-11-26 22:23:52 +0530

    added models in db.ts

---
+++ Backend/src/models/db.ts
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const userSchema = new Schema({
+    name: {type: String, required: true},
+    username: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    profileImagePath: {type: String, required: false, default: ""},
+    department: {type: String, required: true},
+    graduationYear: {type: Number, required: true},
+    bio: {type: String, maxlength: 200},
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'postsModel'
+    }],
+    friends: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'userModel'
+    }],
+    friendRequests: [{ 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: 'User' 
+    }],
+}, { timestamps: true })
+
+const adminSchema = new Schema({
+    name: { type: String, required: true },
+    adminId: { type: String, required: true, unique: true }, 
+    password: { type: String, required: true },
+    role: { type: String, default: 'admin' } 
+}, { timestamps: true });
+
+const postsSchema = new Schema({
+    postedBy: {type: mongoose.Schema.Types.ObjectId, ref:'userModel'},
+    username: {type: String},
+    userImagePath: String,
+    postsImagePath: String,
+    text: {type: String, maxlength: 200},
+    likes: [{type: mongoose.Schema.Types.ObjectId, ref:'userModel'}],
+    // isReported: {type: Boolean},
+    reportedBy: [{type: mongoose.Schema.Types.ObjectId, ref:'userModel'}],
+    // reportedByUsername: [{type: String, unique: true}],
+    // isReportedByUser: {type: Boolean}
+    comments: [{
+        content: String,
+        user: {type: mongoose.Schema.Types.ObjectId, ref:'userModel'},
+        date: {type: Date, default: Date.now}
+    }]
+}, { timestamps: true })
+
+const otpSchema = new Schema({
+    email: { type: String, required: true },
+    otp: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now, expires: 600 } // OTP expires after 10 minutes
+});
+
+export const userModel = mongoose.model('users', userSchema)
+
+export const postModel = mongoose.model('posts', postsSchema)
+
+export const adminModel = mongoose.model('admins', adminSchema)
+
+export const otpModel = mongoose.model('otps', otpSchema)
