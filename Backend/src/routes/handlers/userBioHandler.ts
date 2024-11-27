f5d3cc3c17b80416c1eace26c95c60e7116a1414
Michael Hosamani <michaelhosamani26@gmail.com>
2024-11-27 18:59:14 +0530

    Implemented userBio and viewBio handlers in ts

--- Backend/src/routes/handlers/userBioHandler.ts
+++ Backend/src/routes/handlers/userBioHandler.ts
@@ -1,21 +1,91 @@
 import express, { Express, Router, Request, Response } from "express";
+import { userModel } from "../../models/db";
 
 
 const userBioHanler: Router = express();
 
 // set bio
-userBioHanler.post("/", (req: Request, res: Response) => {
+userBioHanler.post("/", async (req: Request, res: Response): Promise<void> => {
+    try{
+        const userId = req.userId
+        const { content } = req.body
 
+        const user = await userModel.findById(userId)
+
+        if(!user) {
+            res.status(403).json({
+                msg: "user not found"
+            })
+            return
+        }
+
+        user.bio = content
+
+        user.save();
+
+        res.status(200).json({
+            bio: user.bio
+        })
+    }
+    catch(e) {
+        console.error("Error while setting bio")
+    }
 })
+// i think the post request isn't even necessary as it is set as an empty string in the database itself by default
 
 // update bio 
-userBioHanler.put("/", (req: Request, res: Response) => {
+userBioHanler.put("/", async (req: Request, res: Response) => {
+    try{
+        const userId = req.userId
+        const { content } = req.body
 
+        const user = await userModel.findById(userId)
+
+        if(!user) {
+            res.status(403).json({
+                msg: "user not found"
+            })
+            return
+        }
+
+        user.bio = content
+
+        user.save();
+
+        res.status(200).json({
+            bio: user.bio
+        })
+    }
+    catch(e) {
+        console.error("Error while updating bio")
+    }
 })
 
 // delete bio
-userBioHanler.delete("/", (req: Request, res: Response) => {
+userBioHanler.delete("/", async (req: Request, res: Response) => {
+    try{
+        const userId = req.userId
 
+        const user = await userModel.findById(userId)
+
+        if(!user) {
+            res.status(403).json({
+                msg: "user not found"
+            })
+            return
+        }
+
+        user.bio = ""
+
+        user.save();
+
+        res.status(200).json({
+            bio: user.bio
+        })
+    }
+    catch(e) {
+        console.error("Error while updating bio")
+    }
 })
 
 export default userBioHanler;
--- Backend/src/routes/user.ts
+++ Backend/src/routes/user.ts
@@ -6,6 +6,7 @@ import PfpHanler from "./handlers/profilePicHandler";
 import { signupHandler } from "./handlers/signupHandler";
 import { loginHandler } from "./handlers/loginHandler";
 import { authMiddleware } from "../middlewares/auth";
+import viewBioHandler from "./handlers/viewBioHandlers";
 
 const userRouter: Router = Router();
 
@@ -23,6 +24,9 @@ userRouter.use("/friends", authMiddleware, friendHanler)
 
 // user Bio
 userRouter.use("/bio", authMiddleware, userBioHanler)
+
+// view Bio
+userRouter.use("/viewBio", authMiddleware, viewBioHandler)
 
 // PFP
 userRouter.use("/profilePicture", authMiddleware, PfpHanler)
