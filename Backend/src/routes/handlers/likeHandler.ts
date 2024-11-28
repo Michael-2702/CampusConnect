d61a831b2e8b11390f2992c9d04aeb3800157daa
Michael Hosamani <michaelhosamani26@gmail.com>
2024-11-28 16:54:21 +0530

    Implemented likeHandler in ts

--- Backend/src/routes/handlers/likeHandler.ts
+++ Backend/src/routes/handlers/likeHandler.ts
@@ -1,14 +1,89 @@
 import {  Router, Request, Response } from "express";
+import { postModel, userModel } from "../../models/db";
+import { mongo } from "mongoose";
 
 const likeHandler: Router = Router();
 
 // like/unlike a post
-likeHandler.get("/signup", (req: Request, res: Response) => {
+likeHandler.put("/:id", async (req: Request, res: Response): Promise<void> => {
+    try{
+        const userId = req.userId;
+        const postId = req.params.id
 
+        if(!userId)
+            return
+
+        const post = await postModel.findById(postId)
+        if(!post){
+            res.status(401).json({
+                msg: "Post not found"
+            })
+            return
+        }
+
+        const userObjectId = new mongo.ObjectId(userId as string)
+
+        const isLiked = post.likes.includes(userObjectId);
+        if(isLiked){
+            post.likes = post.likes.filter(id => id.toString() !== userId.toString())
+        }
+        else {
+            post.likes.push(userObjectId);
+        }
+
+        await post.save()
+
+        const likedUsers = await userModel.find({ _id: { $in: post.likes } }, 'username profileImagePath');
+
+        res.json({ 
+            message: isLiked ? "Post unliked" : "Post liked", 
+            likes: post.likes,
+            likedUsers: likedUsers
+        });
+    }
+    catch(e) {
+        console.error("Error while liking a post")
+        res.status(401).json({
+            msg: "Error while liking a post"
+        })
+        return
+    }
 })
 
-// get like count
-
 // get users who have like a post
+likeHandler.get("/:id", async (req: Request, res: Response): Promise<void> => {
+    try{
+        const postId = req.params.id
+
+        const post = await postModel.findById(postId)
+
+        if(!post){
+            res.status(401).json({
+                msg: "Post not found"
+            })
+            return;
+        }
+
+        const likedUsers = await userModel.find({ _id: { $in: post.likes }}, "username profileImagePath")
+
+        if(!likedUsers){
+            res.status(401).json({
+                msg: "Liked users not found"
+            })
+            return;
+        }
+
+        res.status(200).json({
+            likedUsers
+        })
+    }
+    catch (e) {
+        console.error("Error while liking a post")
+        res.status(401).json({
+            msg: "Error while liking a post"
+        })
+        return
+    }
+})
 
 export default likeHandler;
